d50a0d49a32eec4952cb084a8f2163270925ed95
Abhishekkrjha30 <abhishekkrjha30@gmail.com>
2025-02-02 23:07:22 +0530

    first commit

---
+++ src/components/ab/dashboard/Dashboard.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable no-unused-vars */
+import { Link, Outlet } from "react-router-dom";
+import Button from "../Button"
+import { Card } from "./Card"
+import dummyData from "../Data/data";
+import { useState,useEffect } from "react";
+
+export const Dashboard = () => {
+    const [data, setData] = useState(dummyData);
+    const [selectedBranch, setSelectedBranch] = useState(null); 
+
+    // Set the first branch as selected by default
+  useEffect(() => {
+    if (data.branches.length > 0) {
+      setSelectedBranch(data.branches[0].id);
+    }
+  }, [data]);
+
+  return (
+    <div className="m-5 mt-28">
+        <h1 className="text-3xl font-bold text-gray-800 mb-6"> Dashboard</h1>
+        
+        {/* Buttons Section */}
+        <div className="w-full flex  bg-gray-100  p-2 md:p-3 rounded-xl shadow-lg my-2 md:my-4 overflow-x-auto overflow-y-hidden " >
+            {data.branches.map((branch,index)=>(
+                <Button 
+                  key={index}
+                  label={branch.name} 
+                  className={`min-w-fit text-xl transition-all duration-300 hover:scale-101 hover:shadow-black hover:shadow-md ${selectedBranch === branch.id ? "transition-all duration-30 scale-101 shadow-black shadow-md" : ""
+                  }`}
+                  onClick={() => setSelectedBranch(branch.id)} 
+                />  
+            ))}
+        </div>
+        {/* batches */}
+        <div className="mt-10">
+          
+            {selectedBranch && data.branches.find((branch) => branch.id === selectedBranch) ?.batches.map((batch) => (
+              <Link key={batch.id} to={`/batchDetail/${batch.id}`}>
+                <Card data={batch} />
+              </Link>
+            ))}
+           
+            
+            
+            
+        </div>
+    </div>
+  )
+}
